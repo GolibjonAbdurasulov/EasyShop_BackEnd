67abd56b034429595ce81468f51c55016c40f56c
Developr <golibjonabdurasulov47@gmail.com>
2025-10-05 17:24:42 +0500

    add [Injectable] to oil tags

--- src/DatabaseBroker/Repositories/Tags/OilProductTagsRepository/OilProductTagsRepository.cs
+++ src/DatabaseBroker/Repositories/Tags/OilProductTagsRepository/OilProductTagsRepository.cs
@@ -1,9 +1,10 @@
 using DatabaseBroker.Repositories.Common;
+using Entity.Attributes;
 using Entity.Models.Product.Tags;
 using Microsoft.EntityFrameworkCore;
 
 namespace DatabaseBroker.Repositories.Tags.OilProductTagsRepository;
-
+[Injectable]
 public class OilProductTagsRepository : RepositoryBase<OilProductTags, long>, IOilProductTagsRepository
 {
     public OilProductTagsRepository(DataContext dbContext) : base(dbContext)
